ee8038f79a07cb8d5b1bbe60a51a4e8672f16d71
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-03-21 13:06:16 +0200

    xxx

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerState = 'fall';
+const dropdown =document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+})
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
@@ -7,17 +13,76 @@ const playerImage = new Image();
 playerImage.src = "css/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
+
+
 let gameFrame = 0;
 const staggerFrames = 5;
+const spiteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    }
+];
+animationStates.ferEach((state, index) =>{
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spiteAnimations[state.name] = frames;
+});
+console.log(animationStates);
 
 //let x =0;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % 6; // here is frame function,
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    let position = Math.floor(gameFrame/staggerFrames) % spiteAnimations[playerState].loc; // here is frame function, not eally understand it :D
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+
+    ctx.drawImage(playerImage, frameX , frameY , spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
   
 
     gameFrame++;
